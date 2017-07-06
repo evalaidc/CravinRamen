271d0868a86423a565e9aeca59150685562c2815
Eva Lai <evalai.dc@gmail.com>
2017-07-06 18:10:20 -0400

    added seed files

--- db/models.js
+++ db/models.js
@@ -1,30 +1,33 @@
 const mongoose = require('./connection.js');
 
-const RamenSchema = new mongoose.Schema (
-  {
-    type: String,
-    nutr_label_pic: String,
-    image_url: String,
-    taste: Number,
-    aroma: Number,
-    noodle_texture: String,
-    overall_score: Number,
-    summary: String,
-    comments: [CommentSchema]
-  },
-  {
-    timestamps: true
-  }
-);
 
+// renamed caption because Comment was not exporting correctly
 const CaptionSchema = new mongoose.Schema (
   {
     author: String,
     comment: String,
-    taste: Number,
-    aroma: Number,
-    overall_score: Number
+    // taste: Number,
+    // aroma: Number,
+    // overall_score: Number
   }, {
+    timestamps: true
+  }
+);
+
+const RamenSchema = new mongoose.Schema (
+  {
+    title: String,
+    origin: String,
+    // nutr_label_pic: String,
+    image_url: String,
+    // taste: Number,
+    // aroma: Number,
+    // noodle_texture: String,
+    // overall_score: Number,
+    summary: String,
+    captions: [CaptionSchema]
+  },
+  {
     timestamps: true
   }
 );
--- db/seeds.js
+++ db/seeds.js
@@ -13,3 +13,49 @@ Caption.remove({}, err => {
     console.log(err);
   }
 });
+
+//
+var spic_sea = new Ramen({
+  "title": "Ottogi Spice Seafood Noodle",
+  "origin": "Korea",
+  "summary": "No that is not a typo. They actually spelled Spicy with an 'e.' If you like fishly smelling ramen, but you can reduce it by adding less of the flavoring.",
+  "image_url": "http://www.theramenrater.com/wp-content/uploads/2011/06/2011_6_30_441_001.jpg"
+})
+var coal = new Ramen({
+  "title": "Sau Tao Non-fried Bamboo Charcoal noodle",
+  "origin": "Hong Kong/China",
+  "summary": "Overall good and flavorful taste. Actually I really enjoyed the noodles and the texture of them and how they didn't go as soggy or flat. It took much longer for it, so it retains more of the good texture of ramen.",
+  "image_url": "http://www.theramenrater.com/wp-content/uploads/2016/12/2016_12_12_2321_001.jpg"
+})
+
+// comment seeds
+var caption1 = new Caption({
+  "author": "Eva",
+  "comment": "I agree this one's noodles were amazing."
+})
+var caption2 = new Caption({
+  "author": "Mike",
+  "comment": "I think the seafood flavor is actually pretty good."
+})
+var caption3 = new Caption({
+  "author": "Joe",
+  "comment": "The noodles and flavor for this one were a little disappointing though. I also don't know what the extra oil packet did."
+})
+
+spic_sea.captions.push(caption2);
+spic_sea.save((err, caption) => {
+  if (err){
+    console.log(err);
+  } else {
+    console.log(caption)
+  }
+});
+
+coal.captions.push(caption1, caption3);
+coal.save((err, caption) => {
+  if (err){
+    console.log(err);
+  } else {
+    console.log(caption)
+  }
+});
