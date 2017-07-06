7ff2bd927acb2c58c8fc820de4ba20008019f0d0
Eva Lai <evalai.dc@gmail.com>
2017-07-06 00:04:31 -0400

    created functions to remove previous data before seeding new data

--- db/seeds.js
+++ db/seeds.js
@@ -1,2 +1,15 @@
 const Ramen = require("./models.js").Ramen;
 const Caption = require("./models.js").Caption;
+
+// removing any stragglers of data from previous runs
+Ramen.remove({}, err => {
+  if (err) {
+    console.log(err);
+  }
+});
+
+Caption.remove({}, err => {
+  if (err){
+    console.log(err);
+  }
+});
