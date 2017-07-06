5fc5b4ecffd512430ab7c8deb503a99e7f0f7b37
Eva Lai <evalai.dc@gmail.com>
2017-07-05 23:05:36 -0400

    created comment model

--- db/models.js
+++ db/models.js
@@ -1,17 +1,29 @@
 const mongoose = require('./connection.js');
 
-const RamenSchema = new mongoose.Schema ({
-  type: String,
-  nutr_label_pic: String,
-  image_url: String,
-  taste: Number,
-  aroma: Number,
-  overall_score: Number,
-  summary: String
-}, {
-  timestamps: true
-})
+const RamenSchema = new mongoose.Schema (
+  {
+    type: String,
+    nutr_label_pic: String,
+    image_url: String,
+    taste: Number,
+    aroma: Number,
+    overall_score: Number,
+    summary: String,
+    comments: [CommentSchema]
+  },
+  {
+    timestamps: true
+  }
+)
 
-const CommentSchema = new mongoose.Schema ({
-
-})
+const CommentSchema = new mongoose.Schema (
+  {
+    author: String,
+    comment: String,
+    taste: Number,
+    aroma: Number,
+    overall_score: Number,
+  }, {
+    timestamps: true
+  }
+)
