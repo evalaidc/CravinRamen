9f51d2efb77324239e73e83160d681fadeed488d
Eva Lai <evalai.dc@gmail.com>
2017-07-05 22:58:56 -0400

    ramen model details done

--- db/models.js
+++ db/models.js
@@ -1,7 +1,17 @@
 const mongoose = require('./connection.js');
 
 const RamenSchema = new mongoose.Schema ({
-  
+  type: String,
+  nutr_label_pic: String,
+  image_url: String,
+  taste: Number,
+  aroma: Number,
+  overall_score: Number,
+  summary: String
 }, {
   timestamps: true
+})
+
+const CommentSchema = new mongoose.Schema ({
+
 })
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Purpose
+
+To provide a place for all ramen lovers whether it be from the restaurant or a store to gather and share their love.
