2491c97a33f540f29d7152e865f8d39cf32e2e3b
Eva Lai <evalai.dc@gmail.com>
2017-07-06 18:44:04 -0400

    express connected and port working

--- server.js
+++ server.js
@@ -7,3 +7,15 @@ const Ramen = require('./db/models.js').Ramen;
 const Caption = require('./db/models.js').Caption;
 
 let app = express()
+
+app.set("port", process.env.Port || 3001);
+app.use("/assets", express.static("public"));
+app.use(parser.json({extended: true}));
+
+app.get("/", function(req, res) {
+})
+
+
+app.listen(app.get("port"), () => {
+  console.log("Port working!");
+});
