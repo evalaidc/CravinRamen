25b76b448209ec141f771d309caf0fa605b01f5f
Eva Lai <evalai.dc@gmail.com>
2017-07-06 18:24:39 -0400

    pulled models from mongoose db to express

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const parser = require('body-parser');
+const mongoose = require('./db/connection.js');
+
+// pulling schemas from models
+const Ramen = require('./db/models.js').Ramen;
+const Caption = require('./db/models.js').Caption;
+
+let app = express()
